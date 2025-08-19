339a9eeb3a8a19104c9604ade6eec0ea0dac4c9e
Azizbek16l <azizbektopilboyev7@gmail.com>
2025-08-19 16:23:26 +0500

    fix'

--- src/App.jsx
+++ src/App.jsx
@@ -14,15 +14,15 @@ function App() {
         
        {
         data.map((item)=>(
-          <div key={item.id}>
+          <div key={item.id} className='card'>
             <div className='img-box'>
-              <img src={item.img} />
-              <button className='buy-btn'>Add to cart</button>
+              <img src={item.img} />             
+            </div>
+             <button className='buy-btn'>Add to cart</button>
               <p>{item.name}</p>
               <p id="price">{item.price} <span id='discount'>{item.aksiya}</span></p>
 
                <span id='reyting'>⭐⭐⭐⭐</span>
-            </div>
           </div>
         ))
        } 
