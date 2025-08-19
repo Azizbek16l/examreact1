0464dd628f33c773023c1ff5c8c88589c68ece05
Azizbek16l <azizbektopilboyev7@gmail.com>
2025-08-19 15:51:51 +0500

    cardlar tuzatildi

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
 import './App.css'
+import data from '../public/data.json'
 
 function App() {
   return (
     <div className='body'>
-  
+    <header>
+      <div className='container'>
+
+      </div>
+    </header>
+    <main>
+      <div className='container'>
+        
+       {
+        data.map((item)=>(
+          <div key={item.id}>
+            <div className='img-box'>
+              <img src={item.img} />
+              <button className='buy-btn'>Add to cart</button>
+              <p>{item.name}</p>
+              <p id="price">{item.price} <span id='discount'>{item.aksiya}</span></p>
+
+               <span id='reyting'>⭐⭐⭐⭐</span>
+            </div>
+          </div>
+        ))
+       } 
+      </div>
+
+    </main>
     </div>
   )
 }
