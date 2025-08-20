13a8866848d267b912d6c70c87bb36ddce58117c
Azizbek16l <azizbektopilboyev7@gmail.com>
2025-08-20 10:59:45 +0500

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 import './App.css'
 import data from '../public/data.json'
-
+import { IoMdSearch } from "react-icons/io";
+import { FaRegUser } from "react-icons/fa";
+import { LuShoppingCart } from "react-icons/lu";
+import { CiHeart } from "react-icons/ci";
 function App() {
   return (
     <div className='body'>
     <header>
       <div className='container'>
+      <h2 id="logo">Exclusive</h2>
+      <ul>
+        <li>Home</li>
+        <li>Contact</li>
+        <li>About</li>
+        <li>Sign Up</li>
 
+      </ul>
+        <ul>
+          <li><input type="text" /><IoMdSearch/></li>
+          <li><CiHeart/></li>
+          <li><LuShoppingCart/></li>
+          <li><FaRegUser/></li>
+        </ul>
+      
       </div>
     </header>
     <main>
